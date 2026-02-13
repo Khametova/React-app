d6b7fe7b86db5e2104a4bb749dc1d4885f0a2b66
Alina Khametova <alinakhametova1@gmail.com>
2026-02-13 09:09:18 +0200

    add class of style male/female

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -2,15 +2,17 @@ import { TbBulb } from "react-icons/tb";
 import styles from "./Article.module.css";
 
 function Article(props) {
+  const articleClassName = `${styles.userWrap} ${props.user.gender === "male" ? styles.maleBorder : styles.femaleBorder} `;
+  const changeColorBorder = () => {};
   console.log(props.user.name);
   return (
-    <article className={styles.userWrap}>
+    <article className={articleClassName}>
       <h2>
         {props.user.name} {props.user.surname}
       </h2>
       <p>{props.user.age}</p>
       <img className={styles.userImg} src={props.user.imgSrc} alt="" />
-      <button>
+      <button onClick={changeColorBorder}>
         <TbBulb />
       </button>
     </article>
